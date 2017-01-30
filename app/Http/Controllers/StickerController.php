42b4bf6f27f56973de096e3c5cda463b0d26eeb4
motikan <motikan2010@yahoo.co.jp>
2017-01-31 00:32:37 +0900

    Edit Welcome page

--- app/Http/Controllers/StickerController.php
+++ app/Http/Controllers/StickerController.php
@@ -8,6 +8,11 @@ use App\Sticker;
 
 class StickerController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
