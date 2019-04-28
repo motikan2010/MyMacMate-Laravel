ae63327779171c3fe41f158fedfce96776c2567a
motikan2010 <motikan2010@yahoo.co.jp>
2019-04-28 23:54:10 +0900

    JSのリファクタリング1 #8

--- public/js/my-list.js
+++ public/js/my-list.js
@@ -4,6 +4,7 @@ $(function () {
     var btnSelector = $(this);
     var productId = $('.product-id-' + btnSelector.val()).val();
 
+    // 公開状態更新リクエスト送信
     $.ajaxSetup({
       headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -15,13 +16,14 @@ $(function () {
       data: {
         'product_id' : productId
       }
-    }).done(function(responseDate, statusCode, xhr) {
+
+    }).done(function(responseDate) {
       if ( responseDate['public_flag'] ) {
         // 公開
         btnSelector.text('Public').addClass('btn-info').removeClass('btn-warning');
       } else {
         // 非公開
-        btnSelector.text('Private').removeClass('btn-info').addClass('btn-warning');
+        btnSelector.text('Private').addClass('btn-warning').removeClass('btn-info');
       }
     });
   });
